31685e7f751faae36d9d652f3d21dfde0c8f01db
葛雄蕤 <420932149@qq.com>
2025-08-08 17:22:17 +0800

    feat:后端新建模块初始化

---
+++ src/main/java/com/gxr/codegenerate/CodeGenerateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gxr.codegenerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeGenerateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeGenerateBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gxr/codegenerate/CodeGenerateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxr.codegenerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeGenerateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
