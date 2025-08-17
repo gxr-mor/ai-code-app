eb005088994b8a26c5cd6a26429c3c139fcf14b9
葛雄蕤 <xiongrui.ge@getech.cn>
2025-08-17 13:32:17 +0800

    feat:1.应用管理的基本功能开发 2.用户应用调用代码生成接口 3.对流式响应结果做封装处理

---
+++ src/main/java/com/gxr/codegenerate/mapper/AppMapper.java
@@ -0,0 +1,14 @@
+package com.gxr.codegenerate.mapper;
+
+
+import com.gxr.codegenerate.model.entity.App;
+import com.mybatisflex.core.BaseMapper;
+
+/**
+ * 应用 映射层。
+ *
+ * @author gxr
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
---
+++ src/main/java/com/gxr/codegenerate/service/AppService.java
@@ -0,0 +1,27 @@
+package com.gxr.codegenerate.service;
+
+import com.gxr.codegenerate.model.dto.app.AppQueryRequest;
+import com.gxr.codegenerate.model.entity.App;
+import com.gxr.codegenerate.model.entity.User;
+import com.gxr.codegenerate.model.vo.AppVo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+/**
+ * 应用 服务层。
+ *
+ * @author gxr
+ */
+public interface AppService extends IService<App> {
+
+    AppVO getAppVO(App app);
+
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+    List<AppVO> getAppVOList(List<App> appList);
+
+    Flux<String> chatToGenCode(Long appId, String message, User loginUser);
+}
