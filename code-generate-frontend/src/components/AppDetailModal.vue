73f5dbb8de6a60bdfc4832057f74d36ca5c540be
葛雄蕤 <xiongrui.ge@getech.cn>
2025-08-25 00:04:44 +0800

    fix: 根据代码类型匹配对应的枚举修复

--- code-generate-frontend/src/components/AppDetailModal.vue
+++ code-generate-frontend/src/components/AppDetailModal.vue
@@ -53,6 +53,7 @@ import { computed } from 'vue'
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons-vue'
 import UserInfo from './UserInfo.vue'
 import { formatTime } from '@/utils/time'
+import { formatCodeGenType } from '@/utils/codeGenTypes.ts'
 
 interface Props {
   open: boolean
