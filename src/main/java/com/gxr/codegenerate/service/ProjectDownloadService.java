e9c6361779313a1f54d3f8b1903e0dd42905b99e
葛雄蕤 <xiongrui.ge@getech.cn>
2025-08-24 22:59:35 +0800

    feat: 在网站应用生成好以后,提供代码的源码下载功能

---
+++ src/main/java/com/gxr/codegenerate/service/ProjectDownloadService.java
@@ -0,0 +1,7 @@
+package com.gxr.codegenerate.service;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface ProjectDownloadService {
+    void downloadProjectAsZip(String projectPath, String downloadFileName, HttpServletResponse response);
+}
